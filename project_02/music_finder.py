2d18652405791bcf14209bba689e1d5219dde1ae
Josh Koo <joshuakoo2019@u.northwestern.edu>
2019-05-30 21:58:15 -0500

    After library sesh 1

--- project_02/music_finder.py
+++ project_02/music_finder.py
@@ -6,31 +6,100 @@ def print_menu():
 ---------------------------------------------------------------------
 Settings / Browse Options
 ---------------------------------------------------------------------
-1 - Select your favorite genres  
-2 - Select your favorite artists 
+1 - Select your favorite genres
+2 - Select your favorite artists
 3 - Discover new music
 4 - Quit
 ---------------------------------------------------------------------
     ''')
 
+genres = []
+artists = []
+
+
 def handle_genre_selection():
-    # 1. Allow user to select one or more genres using the 
+    # 1. Allow user to select one or more genres using the
     #    spotify.get_genres_abridged() function
     # 2. Allow user to store / modify / retrieve genres
-    #    in order to get song recommendations 
+    #    in order to get song recommendations
+    temp = True
+    while temp:
+        temp = False
+        list_of_genres = spotify.get_genres_abridged()
+        i = 0
+        while len(list_of_genres) > i:
+            num = i+1
+            if num in genres:
+                print(str(num) + '. [x] ' + list_of_genres[i])
+            else:
+                print(str(num) + '. [ ] ' + list_of_genres[i])
+            i += 1
+        genre_select = input('Please select up to three genres as a comma-delimited list of numbers. Type "clear" to clear out genres.')
+        if genre_select == 'clear':
+            genres.clear()
+            break
+        temp_genres = []
+        temp_num = ""
+        for char in genre_select:
+            if (char < '0' or char > '9') and char != ',':
+                print("Error: Numbers and commas only")
+                temp = True
+                temp_genres = []
+                temp_num = ""
+                break
+            else:
+                if char == ',':
+                    print(temp_num)
+                    num = int(temp_num)
+                    if num > len(list_of_genres) or num < 1:
+                        print("Not a valid selection")
+                        temp = True
+                        temp_genres = []
+                        temp_num = ""
+                        break
+                    else:
+                        temp_genres.append(num)
+                        temp_num = ""
+                else:
+                    temp_num = temp_num + char
 
-    list_of_genres = spotify.get_genres_abridged()
-    i = 0
-    while len(list_of_genres) > i:
-        num = i+1
-        print(str(num) + '. ' + list_of_genres[i])
-        i += 1
-    print('Please select up to three genres as a comma-delimited list of numbers. Type "clear" to clear out genres.')
+        #For the last value that we miss
+        should_append = False
+        for char in temp_num:
+            if (char < '0' or char > '9') and char != ',':
+                print("Error: Numbers and commas only")
+                temp = True
+                temp_genres = []
+                break
+            elif int(temp_num) > len(list_of_genres) or int(temp_num) < 1:
+                print("Not a valid selection")
+                temp = True
+                temp_genres = []
+                break
+            else:
+                should_append = True
+        if should_append:
+            temp_genres.append(int(temp_num))
+        if len(temp_genres) > 3:
+            print("Please choose up to 3 genres")
+            temp = True
+            temp_genres = []
+            temp_num = ""
+        else:
+            for n in temp_genres:
+                if n not in genres:
+                    if len(genres) < 3:
+                        genres.append(n)
+                    else:
+                        print("You may only have 3 genres")
+                        break
 
-    # if user input is clear, then clear
+
+
+        # if user input is clear, then clear
 
 def handle_artist_selection():
-    # 1. Allow user to search for an artist using 
+    # 1. Allow user to search for an artist using
     #    spotify.get_artists() function
     # 2. Allow user to store / modify / retrieve artists
     #    in order to get song recommendations
@@ -41,9 +110,10 @@ def handle_artist_selection():
     i = 0
     while len(artist_search_results) > i:
         num = i+1
-        print(str(num) + '. ' + str(artist_search_results[i]))
+        print(str(num) + '. ' + str(artist_search_results[i]['name']))
         i += 1
     print('Please select up to three artists as a comma-delimited list of numbers. Type "clear" to clear out artists.')
+    
 
 # make sure it's simplified??
 
